a6d0eba554abbdb1bf125f52bfb3c7e1ee5060f0
Angela Stephens <angelastephens317@gmail.com>
2020-06-08 02:23:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,7 +93,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season,holiday_values|
     array= []
     holiday_hash.each do |season, name|
     name.each do |holiday, stuff|
